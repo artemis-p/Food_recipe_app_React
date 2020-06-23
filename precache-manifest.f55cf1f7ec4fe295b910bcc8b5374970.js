c829d72b1aa44afc6902bd5f3a86f1eb2ab4c436
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-06-23 14:57:01 +0100

    Updates

---
+++ precache-manifest.f55cf1f7ec4fe295b910bcc8b5374970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baed94ac137a27eeb748322f1065a809",
+    "url": "/Food_recipe_app_React/index.html"
+  },
+  {
+    "revision": "fe198a6ea97fcd9c351d",
+    "url": "/Food_recipe_app_React/static/css/main.828160d2.chunk.css"
+  },
+  {
+    "revision": "e84de66d17fc65e953c9",
+    "url": "/Food_recipe_app_React/static/js/2.df3c97c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Food_recipe_app_React/static/js/2.df3c97c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe198a6ea97fcd9c351d",
+    "url": "/Food_recipe_app_React/static/js/main.4655bbda.chunk.js"
+  },
+  {
+    "revision": "f336c23cff3f0cb35c11",
+    "url": "/Food_recipe_app_React/static/js/runtime-main.65999ed4.js"
+  }
+]);
